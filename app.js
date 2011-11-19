893405dc5cb3925370e268944595bbc97bda46a5
dreamlx <dreamlinx@gmail.com>
2011-11-19 20:24:53 +0800

    app new actions test

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 Ext.application({
     name: 'Sencha',
+ 
     launch: function() {
-        alert('launched');
+        Ext.create("Ext.TabPanel", {
+            fullscreen: true,
+            tabBarPosition: 'bottom', //tabBar设在下方
+ 
+            items: [
+                {
+                    title: 'Home',
+                    iconCls: 'home', //添加Tab的图标
+                    html: [//添加内容
+                        
+                        '<h1>Welcome to Sencha Touch</h1>',
+                        "<p>You're creating the Getting Started app. This demonstrates how ",
+                        "to use tabs, lists and forms to create a simple app</p>",
+                        '<h2>Sencha Touch (2.0.0pr1)</h2>'
+                    ].join("")
+                }
+            ]
+        });
     }
 });
